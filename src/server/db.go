72797b9e7fead9c812452e740b9bc39475bad806
kotto5 <kotto555555@gmail.com>
2024-04-07 09:54:28 +0900

    initialize の度に automigrate 呼んでると、query が膨大に行われてたので、init connect を分割

--- src/server/db.go
+++ src/server/db.go
@@ -135,11 +135,58 @@ func connectToDB() (*gorm.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	db.AutoMigrate(&User{})
-	db.AutoMigrate(&M5Stick{})
-	db.AutoMigrate(&Activity{})
-	db.AutoMigrate(&Location{})
-	db.AutoMigrate(&Role{})
+	db.AutoMigrate(&User{}, &M5Stick{}, &Activity{}, &Location{}, &Role{})
+	return db, nil	
+}
+
+func seed(db *gorm.DB) error {
+	// Create a new user
+	users := []User{{UID: "foo", Login: "kakiba"}, {UID: "bar", Login: "tanemura"}}
+	for _, user := range users {
+		if result := db.Create(&user); result.Error != nil {
+			return result.Error
+		}
+	}
+
+	locations := []Location{{Name: "F1"}, {Name: "F2"}}
+	for _, location := range locations {
+		if result := db.Create(&location); result.Error != nil {
+			return result.Error
+		}
+	}
+
+	roles := []Role{{Name: "Cleaning"}, {Name: "UsingShower"}}
+	for _, role := range roles {
+		if result := db.Create(&role); result.Error != nil {
+			return result.Error
+		}
+	}
+
+	m5Sticks := []M5Stick{{Mac: "00:00:00:00:00:00", RoleId: 1, LocationId: 1}, {Mac: "11:11:11:11:11:11", RoleId: 2, LocationId: 2}}
+	for _, m5Stick := range m5Sticks {
+		if result := db.Create(&m5Stick); result.Error != nil {
+			return result.Error
+		}
+	}
+
+	activities := []Activity{
+		{UserID: 1, M5StickID: 1},
+		{UserID: 2, M5StickID: 2},
+	}
+	for _, activity := range activities {
+		if result := db.Create(&activity); result.Error != nil {
+			return result.Error
+		}
+	}
+
+	return nil
+}
+
+func connectToDB() (*gorm.DB, error) {
+	dsn, err := getDSN()
+	if err != nil {
+		return nil, err
+	}
 	return db, nil
 }
 
@@ -150,134 +197,4 @@ func getDSN() (string, error) {
 		return "", fmt.Errorf("DB_DSN environment variable is not set")
 	}
 	return dsn, nil
-}
-
-// connectToDatabase はデータベースへの接続を試みます。
-func connectToDatabase(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
-}
-
-// verifyConnection はデータベースへの接続を確認します。
-func verifyConnection(db *sql.DB) error {
-	if err := db.Ping(); err != nil {
-		return err
-	}
-	return nil
-}
-
-// InsertUser は新しいユーザーをusersテーブルに挿入します。
-func InsertUser(db *sql.DB, uid, login string) error {
-	_, err := db.Exec("INSERT INTO users (uid, login) VALUES (?, ?)", uid, login)
-	if err != nil {
-		return err
-	}
-	fmt.Println("Inserted a new user into the users table.")
-	return nil
-}
-
-func GetUserByUid(db *sql.DB, uid string) (User, error) {
-	var user User
-	query := "SELECT id, uid, login FROM users WHERE uid = ?"
-	err := db.QueryRow(query, uid).Scan(&user.ID, &user.UID, &user.Login)
-	if err != nil {
-		return user, err
-	}
-	return user, nil
-}
-
-func GetM5StickByMac(db *sql.DB, mac string) (M5Stick, error) {
-	var m5Stick M5Stick
-	query := "SELECT id, mac FROM m5sticks WHERE mac = ?"
-	err := db.QueryRow(query, mac).Scan(&m5Stick.ID, &m5Stick.Mac)
-	if err != nil {
-		return m5Stick, err
-	}
-	return m5Stick, nil
-}
-
-func InsertActivity(db *sql.DB, user_id, m5Stick_id int) error {
-	ts := time.Now().Unix()
-	query := "INSERT INTO activities (user_id, m5stick_id, timestamp) VALUES ($1, $2, $3)"
-	_, err := db.Exec(query, user_id, m5Stick_id, ts)
-	if err != nil {
-		return err
-	}
-	fmt.Println("Inserted a new activity into the activities table.")
-	return nil
-}
-
-// GetUsers はusersテーブルから全てのユーザーを取得し、表示します。
-func GetUsers(db *sql.DB) ([]User, error) {
-	rows, err := db.Query("SELECT id, uid, login FROM users")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var users []User
-	fmt.Println("Users:")
-	for rows.Next() {
-		var u User
-		if err := rows.Scan(&u.ID, &u.UID, &u.Login); err != nil {
-			return nil, err
-		}
-		users = append(users, u)
-		fmt.Printf("ID: %d, UID: %s, Login: %s\n", u.ID, u.UID, u.Login)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return users, nil
-}
-
-// getCleanData はデータベースから条件に合う掃除データを取得します。/cleanings?start=[UNIXtime]&end=[UNIXtime]
-func getCleanData(c *gin.Context, db *gorm.DB) ([]Activity, error) {
-	start := c.Query("start")
-    end := c.Query("end")
-
-	startInt, err := strconv.ParseInt(start, 10, 64)
-    if err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid start timestamp"})
-        return nil, err
-    }
-
-    endInt, err := strconv.ParseInt(end, 10, 64)
-    if err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid end timestamp"})
-        return nil, err
-    }
-
-	if startInt > endInt {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid time range"})
-		return nil, err
-	}
-
-	rows, err := db.Table("activities").Where("created_at >= ? AND created_at <= ?", startInt, endInt).Rows()
-	// rows, err := db.Query("SELECT * FROM activities WHERE timestamp >= ? AND timestamp <= ?", startInt, endInt)
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Database query failed", "message": err})
-        return nil, err
-    }
-	defer rows.Close()
-    // Scan the rows into a slice
-    var Activitys []Activity
-    for rows.Next() {
-		var activity Activity
-        err := rows.Scan(&activity.ID, &activity.UserID, &activity.M5StickID, &activity.CreatedAt)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to scan row", "message": err})
-            return nil, err
-		}
-		Activitys = append(Activitys, Activity{
-			ID: activity.ID,
-			UserID: activity.UserID,
-			M5StickID: activity.M5StickID,
-			CreatedAt: activity.CreatedAt,
-		})
-    }
-	return Activitys, nil
 }
