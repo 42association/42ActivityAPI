786c06a12e0772c59438363510dcf1adc123218e
ShotaTanemura <shota0711tane@gmaail.com>
2024-05-13 18:57:40 +0900

    getQueryAboutDate returns date string taking query parameter as an argument

--- api/src/main.go
+++ api/src/main.go
@@ -113,6 +113,16 @@ func getCleanData(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, Activities)
 }
+func getQueryAboutDate(c *gin.Context) (string, error) {
+	date := c.Query("date")
+	if date == "" {
+		date = time.Now().Format("2006/01/02")
+	}
+	if !regexp.MustCompile(`^\d{4}/\d{2}/\d{2}$`).MatchString(date) {
+		return "", errors.New("Invalid date format. It should be in YYYY/MM/DD format")
+	}
+	return date, nil
+}
 
 func getQueryAboutTime(c *gin.Context) (int64, int64, error) {
 	var start_time int64
