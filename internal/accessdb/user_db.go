c55f9e4ff3512ba4c784e65f62d58da8120056d1
syamasaw <17191sawa@gmail.com>
2024-06-07 21:11:29 +0900

    fix

--- internal/accessdb/user_db.go
+++ internal/accessdb/user_db.go
@@ -57,3 +57,26 @@ func EditUserInDB(uid string, login string, wallet string) error {
 	}
 	return nil
 }
+
+// Receives uid, login, and wallet, and if the same login does not exist in the DB, adds a new user.
+func AddUserToDB(uid string, login string, wallet string) error {
+	db, err := ConnectToDB()
+	if err != nil {
+		return err
+	}
+
+	var existingUser User
+	if err := db.Where("login = ?", login).First(&existingUser).Error; err != nil {
+		if err != gorm.ErrRecordNotFound {
+			return err
+		}
+	} else {
+		return errors.New("User already exists")
+	}
+	user := User{UID: uid, Login: login, Wallet: wallet}
+
+	if result := db.Create(&user); result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
