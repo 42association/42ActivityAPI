f65d0a990c586d4208909ecbed00efec78c885d8
syamasaw <17191sawa@gmail.com>
2024-06-26 17:10:25 +0900

    update callback

--- cmd/ft_activity_api/main.go
+++ cmd/ft_activity_api/main.go
@@ -10,6 +10,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
+	"strings"
 )
 
 func main() {
@@ -22,8 +24,6 @@ func main() {
 
 	router := gin.Default()
 
-	router.Use(checkauth.CheckAuthHeader())
-
 	router.LoadHTMLGlob("web/templates/*")
 
 	// CORS Settings
@@ -33,26 +33,30 @@ func main() {
 
 	router.GET("/", ShowIndexPage)
 	router.GET("/new", RedirectToIndexWithUID)
-	router.GET("/callback", ShowCallbackPage)
+	// router.GET("/callback", ShowCallbackPage)
+	router.GET("/callback", handleCallback)
 
-	router.POST("/receive-uid", handlers.HandleUIDSubmission)
+	authGroup := router.Group("/", checkauth.CheckAuthHeader())
+	{
+		authGroup.POST("/receive-uid", handlers.HandleUIDSubmission)
 
-	router.GET("/shifts", handlers.GetShiftData)
-	router.POST("/shifts", handlers.AddShiftData)
-	router.POST("/shifts/exchange", handlers.ExchangeShiftData)
-	router.DELETE("/shifts", handlers.DeleteShiftData)
+		authGroup.GET("/shifts", handlers.GetShiftData)
+		authGroup.POST("/shifts", handlers.AddShiftData)
+		authGroup.POST("/shifts/exchange", handlers.ExchangeShiftData)
+		authGroup.DELETE("/shifts", handlers.DeleteShiftData)
 
-	router.POST("/activities", handlers.AddActivity)
-	router.GET("/activities/cleanings", handlers.GetActivityCleanData)
+		authGroup.POST("/activities", handlers.AddActivity)
+		authGroup.GET("/activities/cleanings", handlers.GetActivityCleanData)
 
-	router.POST("/roles", handlers.AddRole)
+		authGroup.POST("/roles", handlers.AddRole)
 
-	router.POST("/locations", handlers.AddLocation)
+		authGroup.POST("/locations", handlers.AddLocation)
 
-	router.POST("/m5sticks", handlers.AddM5Stick)
+		authGroup.POST("/m5sticks", handlers.AddM5Stick)
 
-	router.POST("/users", handlers.AddUsers)
-	router.PUT("/users", handlers.EditUser)
+		authGroup.POST("/users", handlers.AddUsers)
+		authGroup.PUT("/users", handlers.EditUser)
+	}
 
 	router.Run(":" + os.Getenv("PORT"))
 }
@@ -76,4 +80,40 @@ func RedirectToIndexWithUID(c *gin.Context) {
 
 func ShowCallbackPage(c *gin.Context) {
 	c.HTML(http.StatusOK, "callback.html", nil)
+}
+
+func handleCallback(c *gin.Context) {
+	uid := c.Query("uid")
+	code := c.Query("code")
+	if uid == "" || code == "" {
+		c.HTML(http.StatusBadRequest, "response.html", gin.H{"Message": "NFCタグとログインの紐付けに失敗しました。管理者に問い合わせてください。"})
+		return
+	}
+	client := &http.Client{}
+	data := map[string]string{ "uid": uid, "code": code }
+	body, err := json.Marshal(data)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "response.html", gin.H{"Message": "NFCタグとログインの紐付けに失敗しました。管理者に問い合わせてください。"})
+		return
+	}
+	req, err := http.NewRequest("POST", "http://localhost:4242/receive-uid", strings.NewReader(string(body)))
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "response.html", gin.H{"Message": "NFCタグとログインの紐付けに失敗しました。管理者に問い合わせてください。"})
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer " + os.Getenv("API_KEY"))
+	res, err := client.Do(req)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "response.html", gin.H{"Message": "NFCタグとログインの紐付けに失敗しました。管理者に問い合わせてください。"})
+		return
+	}
+	defer res.Body.Close()
+	if res.StatusCode == 200 {
+		c.HTML(http.StatusOK, "response.html", gin.H{"Message": "NFCタグとログインの紐付けが完了しました。ブラウザを閉じてください。"})
+	} else if res.StatusCode == 409 {
+		c.HTML(http.StatusConflict, "response.html", gin.H{"Message": "既に登録されているログインです。ブラウザを閉じてださい。"})
+	} else {
+		c.HTML(res.StatusCode, "response.html", gin.H{"Message": "NFCタグとログインの紐付けに失敗しました。管理者に問い合わせてください。"})
+	}
 }
