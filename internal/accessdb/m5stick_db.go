e41df39b1d68a7acb1af707d2fae00b4cab84691
syamasaw <17191sawa@gmail.com>
2024-07-24 17:45:10 +0900

    /m5sticksのステータスを更新

--- internal/accessdb/m5stick_db.go
+++ internal/accessdb/m5stick_db.go
@@ -9,35 +9,41 @@ import (
 Receives the MAC address, role name, and location name,
 and if the same MAC address does not exist in the DB, adds a new M5stick
 */
-func AddM5StickToDB(mac string, roleName string, locationName string) error {
+func AddM5StickToDB(mac string, roleName string, locationName string) (error, int) {
 	db, err := ConnectToDB()
 	if err != nil {
-		return err
+		return err, 500
 	}
 
 	var existingM5Stick M5Stick
 	if err := db.Where("mac = ?", mac).First(&existingM5Stick).Error; err != nil {
 		if err != gorm.ErrRecordNotFound {
-			return err
+			return err, 500
 		}
 	} else {
-		return errors.New("M5Stick already exists")
+		return errors.New("M5Stick already exists"), 409
 	}
 
 	var role Role
 	if err := db.Where("name = ?", roleName).First(&role).Error; err != nil {
-		return err
+		if err == gorm.ErrRecordNotFound {
+			return errors.New("Role does not exist"), 404
+		}
+		return err, 500
 	}
 
 	var location Location
 	if err := db.Where("name = ?", locationName).First(&location).Error; err != nil {
-		return err
+		if err == gorm.ErrRecordNotFound {
+			return errors.New("Location does not exist"), 404
+		}
+		return err, 500
 	}
 
 	m5Stick := M5Stick{Mac: mac, RoleId: role.ID, LocationId: location.ID}
 
 	if result := db.Create(&m5Stick); result.Error != nil {
-		return result.Error
+		return result.Error, 500
 	}
-	return nil
+	return nil, 200
 }
--- internal/handlers/m5sticks.go
+++ internal/handlers/m5sticks.go
@@ -17,15 +17,23 @@ func AddM5Stick(c *gin.Context) {
 	var requestData M5StickRequestData
 
 	if err := c.BindJSON(&requestData); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad request."})
 		return
 	}
 	if requestData.Mac == "" || requestData.RoleName == "" || requestData.LocationName == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "All parameters are required"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad request."})
 		return
 	}
-	if err := accessdb.AddM5StickToDB(requestData.Mac, requestData.RoleName, requestData.LocationName); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	if err, status := accessdb.AddM5StickToDB(requestData.Mac, requestData.RoleName, requestData.LocationName); err != nil {
+		if status == http.StatusConflict {
+			c.JSON(http.StatusConflict, gin.H{"error": "Conflict."})
+			return
+		}
+		if status == http.StatusNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Not Found."})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error."})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"mac": requestData.Mac, "role": requestData.RoleName, "location": requestData.LocationName})
